152eb1084127f9c859cd642adaf2b2ffb504fd26
Murat Çorlu <muratcorlu@gmail.com>
2010-08-04 19:01:25 +0300

    Skeletons of main methods added

---
+++ library/view.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * BOO Framework
+ * 
+ * view.php file
+ *
+ * Executes HTML templates
+ */
+
+class boo_view {
+	
+    /**
+     * generateView
+     *
+     * Generates a page by given viewId
+     *
+     * @viewId string View ID
+     */
+	public function generateView ( $viewId )
+	{
+        // Read view configuration
+	}
+
+    /**
+     * getBlockContent
+     *
+     * Generates HTML of a block by given block comfiguration
+     *
+     * @block array Block configuration
+     */
+    public function getBlockContent ( $block )
+    {
+    }
+
+    /**
+     * getWidgetContent
+     *
+     * Generates HTML of a widget by given widget configuration
+     *
+     * @widget array Widget configuration
+     */
+    public function getWidgetContent ( $widget )
+    {
+    }
+
+}
